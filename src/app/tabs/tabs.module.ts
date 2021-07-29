4f2d282f2f68977f5eabe793f752dfa467c69185
sivaganesh193 <67482763+sivaganesh193@users.noreply.github.com>
2021-07-29 19:55:56 +0530

    bar graph

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { GaugeChartModule } from 'angular-gauge-chart';
 import { TabsPageRoutingModule } from './tabs-routing.module';
+import { NgApexchartsModule } from "ng-apexcharts";
+
 
 import { TabsPage } from './tabs.page';
 
@@ -13,7 +15,8 @@ import { TabsPage } from './tabs.page';
     CommonModule,
     FormsModule,
     TabsPageRoutingModule,
-    GaugeChartModule
+    GaugeChartModule,
+    NgApexchartsModule
   ],
   declarations: [TabsPage]
 })
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { QUESTIONS } from './questions';
 import { Question} from './question';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexXAxis,
+  ApexPlotOptions
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  xaxis: ApexXAxis;
+};
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
+
 export class TabsPage implements OnInit{
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+  
   startTest = false;
-  result = false;
+  result = true;
   totalQuestions = 0;
   score = 0;
   currentType: string;
@@ -39,7 +59,69 @@ export class TabsPage implements OnInit{
   };
   public finalScore = 0;
 
-  constructor() {}
+  categoryScores: any = {
+    psychometric: {
+      score: 0,
+      total: 0
+    },
+    aptitude: {
+      score: 0,
+      total: 0
+    },
+    creativity: {
+      score: 0,
+      total: 0
+    },
+    adaptability: {
+      score: 0,
+      total: 0
+    },
+    verbal: {
+      score: 0,
+      total: 0
+    },
+    teamwork: {
+      score: 0,
+      total: 0
+    }
+  };
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "basic",
+          data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
+        }
+      ],
+      chart: {
+        type: "bar",
+        height: 350
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      xaxis: {
+        categories: [
+          "South Korea",
+          "Canada",
+          "United Kingdom",
+          "Netherlands",
+          "Italy",
+          "France",
+          "Japan",
+          "United States",
+          "China",
+          "Germany"
+        ]
+      }
+    };
+  }
   getCategoryQuestions(category: string){
     const ques = QUESTIONS.filter(q => q.category === category);
     return ques;
@@ -71,15 +153,21 @@ export class TabsPage implements OnInit{
     }
     this.totalQuestions ++;
     this.currentCategoryQuestions = this.currentCategoryQuestions.filter(q => q.id !== this.currentQuestion.id);
+    if(this.currentQuestion.level === 'hard') {
+      this.categoryScores[this.currentQuestion.category].total+=75;
+    }
+    else {
+      this.categoryScores[this.currentQuestion.category].total+=25;
+    }
     if(this.currentAnswer === this.currentQuestion.answer){
       this.score = this.currentQuestion.level === 'easy'? this.score + 1 : this.score + 2;
       if(this.currentQuestion.level === 'hard'){
         this.hardAnswer++;
-        this.score += 2;
+        this.categoryScores[this.currentQuestion.category].score+=75;
       }
       if(this.currentQuestion.level === 'easy'){
         this.easyAnswer++;
-        this.score += 1;
+        this.categoryScores[this.currentQuestion.category].score+=25;
         this.currentCategoryQuestions = this.currentCategoryQuestions.filter(q => q.level === 'hard');
         if(this.currentCategoryQuestions.length > 0){
           nextCategory = false;
@@ -98,6 +186,8 @@ export class TabsPage implements OnInit{
     if(this.result){
       this.finalScore = ((this.easyAnswer / this.easyQuestions) * 25) + ((this.hardAnswer / this.hardQuestions) * 75);
       this.bottomLabel =  this.finalScore.toFixed(2).toString() + '%';
+      console.log(this.categoryScores);
+      
     }
     this.currentAnswer = '';
 
